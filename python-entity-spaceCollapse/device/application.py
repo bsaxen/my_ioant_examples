342f3181d947de86edef6b9a717a1c684cd3f08c
bsaxen <benny.saxen@gmail.com>
2018-05-04 09:56:14 +0200

    Update application.py
    bugg correction

--- python-entity-spaceCollapse/device/application.py
+++ python-entity-spaceCollapse/device/application.py
@@ -64,23 +64,27 @@ def on_message(topic, message):
     msg_type_string = ioant.get_message_type_name(topic['message_type'])
     #print message.value
     #print topic["message_type"]
+    ok = 0
     if(topic['message_type'] == 4): # temperature
        unit = "celcius"
+       ok = 1 
     if(topic['message_type'] == 8): # electric power
        unit = "watt"
-    scUrl = "http://spacecollapse.simuino.com/scServer.php?"
-    scUrl = scUrl + "type=" + msg_type_string
-    scUrl = scUrl + "&label=" + topic["global"]+'_'+topic["local"]+'_'+topic["client_id"]+'_'+str(topic["stream_index"])
-    scUrl = scUrl + "&value=" + "{0:.2f}".format(message.value)
-    scUrl = scUrl + "&unit=" + unit
-    scUrl = scUrl + "&datetime=" + datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
-    #scUrl = scUrl + "&description=" +
-    #%22This%20is%20a%20measurement%20in%20my%20house%22"
-    print scUrl
-    r = requests.get(scUrl)
-    print r.text
-    #publish_ioant_message(r.text)
-    print r.content
+       ok = 1
+    if ok == 1:
+       scUrl = "http://spacecollapse.simuino.com/scServer.php?"
+       scUrl = scUrl + "type=" + msg_type_string
+       scUrl = scUrl + "&label=" + topic["global"]+'_'+topic["local"]+'_'+topic["client_id"]+'_'+str(topic["stream_index"])
+       scUrl = scUrl + "&value=" + "{0:.2f}".format(message.value)
+       scUrl = scUrl + "&unit=" + unit
+       scUrl = scUrl + "&datetime=" + datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+       #scUrl = scUrl + "&description=" +
+       #%22This%20is%20a%20measurement%20in%20my%20house%22"
+       print scUrl
+       r = requests.get(scUrl)
+       print r.text
+       #publish_ioant_message(r.text)
+       print r.content
 
 
 def on_connect():
